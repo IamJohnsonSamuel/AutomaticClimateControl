cb0308db2f4b0c5fa7f22ada2038e27b8fb41aaf
jho1cob <samuel.johnson@in.bosch.com>
2023-10-31 11:33:39 +0530

    moved sensor function outside of temparature calculator

--- TempCalculator.cpp
+++ TempCalculator.cpp
@@ -1,13 +1,13 @@
 #include "TempCalculator.h"
 
-int TempCalculator::getOutsideTemp()
+int OutsideTemp::getOutsideTemp()
 {
     int outside = 0;
 
     return outside;
 }
 
-int TempCalculator::getOccupancyCount()
+int OccupencySensor::getOccupancyCount()
 {
     int occupancy = 0;
 
@@ -22,8 +22,8 @@ int TempCalculator::getTempFromOutsideAndOccupancy(int outside, int occupancy)
 
 int TempCalculator::getTemperature()
 {
-    int outsideTemp = getOutsideTemp();
-    int occupancyCount = getOccupancyCount();
+    int outsideTemp = outsideTempObj.getOutsideTemp();
+    int occupancyCount = occupencyObj.getOccupancyCount();
 
     int currentTemp = getTempFromOutsideAndOccupancy(outsideTemp, occupancyCount);
 
--- TempCalculator.h
+++ TempCalculator.h
@@ -4,11 +4,26 @@
 #ifndef __TEMP_CALCULATOR__
 #define __TEMP_CALCULATOR__
 
+class OutsideTemp
+{
+
+public:
+    int getOutsideTemp();
+};
+
+class OccupencySensor
+{
+
+public:
+    int getOccupancyCount();
+};
+
 class TempCalculator
 {
 private:
-    int getOutsideTemp();
-    int getOccupancyCount();
+    OutsideTemp outsideTempObj;
+    OccupencySensor occupencyObj;
+
     int getTempFromOutsideAndOccupancy(int outside, int occupancy);
 
 public:
